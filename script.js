ab56c24a99b516e08eb4132e54da41256e0b62a7
Ping <dev.bastianabel@gmail.com>
2024-11-28 13:56:56 +0100

    animation and style rendering vor x and o added

--- script.js
+++ script.js
@@ -34,9 +34,71 @@ function render() {
 function setSymbols(cellValue) {
     let cellContent = '';
     if (cellValue === 'circle') {
-        cellContent = 'o'; // Kreis
+        cellContent = generateCircle();
     } else if (cellValue === 'cross') {
-        cellContent = 'x'; // Kreuz
+        cellContent = generateCross();
     }
     tableHTML += `<td>${cellContent}</td>`;
+}
+
+function generateCircle() {
+    return `
+        <svg width="64" height="64" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+            <circle 
+                cx="32" 
+                cy="32" 
+                r="27" 
+                fill="none" 
+                stroke="#0135A7" 
+                stroke-width="5" 
+                stroke-dasharray="170" 
+                stroke-dashoffset="170"
+                style="animation: loadCircle 250ms linear forwards;">
+            </circle>
+        </svg>
+        <style>
+            @keyframes loadCircle {
+                to {
+                    stroke-dashoffset: 0;
+                }
+            }
+        </style>
+    `;
+}
+
+function generateCross() {
+    return `
+        <svg width="64" height="64" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+            <line 
+                x1="10" y1="10" x2="54" y2="54" 
+                stroke="#7C0707" 
+                stroke-width="5" 
+                stroke-linecap="round"
+                stroke-dasharray="62.5"
+                stroke-dashoffset="62.5"
+                style="animation: drawLine1 250ms linear forwards;">
+            </line>
+            <line 
+                x1="54" y1="10" x2="10" y2="54" 
+                stroke="#7C0707" 
+                stroke-width="5" 
+                stroke-linecap="round"
+                stroke-dasharray="62.5"
+                stroke-dashoffset="62.5"
+                style="animation: drawLine2 250ms linear 250ms forwards;">
+            </line>
+        </svg>
+        <style>
+            @keyframes drawLine1 {
+                to {
+                    stroke-dashoffset: 0;
+                }
+            }
+            @keyframes drawLine2 {
+                to {
+                    stroke-dashoffset: 0;
+                }
+            }
+        </style>
+    `;
 }
